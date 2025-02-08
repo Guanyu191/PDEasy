b10d5486059d748dbb56c2f638db6f2df30e01c5
Guanyu191 <guanyu191@163.com>
2025-02-08 13:29:24 +0800

    move _Dataset base class to dataset_base.py

--- dataset/rectangle.py
+++ dataset/rectangle.py
@@ -2,37 +2,13 @@
 Descripttion: 
 Author: Guanyu
 Date: 2025-02-07 22:36:57
-LastEditTime: 2025-02-08 08:31:32
+LastEditTime: 2025-02-08 13:27:13
 FilePath: \PDEasy\dataset\rectangle.py
 '''
 import numpy as np
-import torch
 
-from ..utils import sample_on_line
-
-
-class _Dataset():
-    def __init__(self):
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.data_dict = {}  # data dictionary 将数据存放于字典
-
-    def array2tensor(self):
-        for k, v in self.data_dict.items():
-            self.data_dict[k] = torch.from_numpy(v).float().to(self.device)
-
-    def tensor2array(self):
-        for k, v in self.data_dict.items():
-            self.data_dict[k] = v.detach().cpu().numpy()
-
-    def statistic(self):
-        self.data_dict["mean"] = self.data_dict["X_res"].mean(axis=0)
-        self.data_dict["std"] = self.data_dict["X_res"].std(axis=0)
-
-    def update_dataset(self):
-        NotImplementedError
-
-    def external_data(self):
-        NotImplementedError
+from dataset.dataset_base import _Dataset
+from utils import sample_on_line
 
 
 class Dataset1D(_Dataset):
