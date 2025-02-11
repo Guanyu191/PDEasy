3973999e6a0d1c03957710c515365a929f7761e1
Guanyu191 <guanyu191@163.com>
2025-02-11 17:54:17 +0800

    expanded relative error function.

--- utils/relative_error.py
+++ utils/relative_error.py
@@ -2,7 +2,7 @@
 Descripttion: 
 Author: Guanyu
 Date: 2025-02-08 14:20:45
-LastEditTime: 2025-02-11 11:49:39
+LastEditTime: 2025-02-11 16:26:55
 '''
 import numpy as np
 import torch
@@ -17,23 +17,69 @@ def relative_error_of_solution(pinn, ref_data, num_sample=None):
     pinn.eval()
     device = next(pinn.network_solution.parameters()).device
 
-    X, u = ref_data
+    # 这里的 X 一定是要整体的形式，用以直接喂给网络
+    # 这里的 sol 一定要是整体的形式，为了先采样 num_sample
+    X, sol = ref_data
+    if isinstance(X, np.ndarray):
+        if X.ndim == 1:
+            X = X.reshape(-1, 1)
+    elif isinstance(X, (list, tuple)):
+        if X[0].ndim == 1:
+            X = np.stack(X, axis=1)
+        elif X[0].ndim == 2:
+            X = np.concatenate(X, axis=1)
+    else:
+        raise
+
+    if isinstance(sol, np.ndarray):
+        if sol.ndim == 1:
+            sol = sol.reshape(-1, 1)
+    elif isinstance(sol, (list, tuple)):
+        if sol[0].ndim == 1:
+            sol = np.stack(sol, axis=1)
+        elif sol[0].ndim == 2:
+            sol = np.concatenate(sol, axis=1)
+        else:
+            raise
+    else:
+        raise
 
     # 如果设置了 num_sample 则随机采样以便快速计算 u 的 relative l2 error
     if num_sample is not None:
         idx = np.random.choice(X.shape[0], num_sample, replace=False)
         X = X[idx]
-        u = u[idx]
+        sol = sol[idx]
 
-    # 计算 u 的 relative l2 error
-    u_pred = pinn.net_sol(
+    # 喂 X 给网络，计算 sol_pred
+    sol_pred = pinn.net_sol(
         torch.from_numpy(X).float().to(device)
     )
-    u_pred = u_pred.detach().cpu().numpy().flatten()
-    error_u = rl2_error(u_pred, u)
+
+    # 前面已经将 sol 转换为 ndim=2 的 ndarray 形式
+    # 下面希望拆分成逐列的形式，用于计算 relative l2 error
+    # 如果 sol 只有一列，则直接 flatten
+    # 如果 sol 是多列的，则分开 sol 为逐列的形式，再 flatten
+    if sol.shape[1] == 1:
+        u = sol.flatten()
+    elif sol.shape[1] > 1:
+        u = [sol[:, [i]].flatten() for i in range(sol.shape[1])]
+    else:
+        raise
+
+    # 因为 net_sol 一定是以单列的形式返回的
+    # 所以，想要知道有几个反演参数要计算，则判断它是否为 list/tuple
+    # 如果是，则逐列计算 error，如果不是，则直接计算 error
+    if isinstance(sol_pred, (list, tuple)):
+        u_pred = [u_pred.detach().cpu().numpy().flatten() for u_pred in sol_pred]
+        error_u = [rl2_error(u_pred, u) for u_pred, u in zip(u_pred, u)]
+    elif isinstance(sol_pred, torch.Tensor):
+        u_pred = sol_pred.detach().cpu().numpy().flatten()
+        error_u = rl2_error(u_pred, u)
+    else:
+        raise
 
     pinn.train()
-    
+    # 返回 u 的误差和预测值
     return error_u, u_pred
 
 
@@ -65,6 +111,8 @@ def relative_error_of_parameter(pinn, ref_data, num_sample=None, column_index=No
             param = np.concatenate(param, axis=1)
         else:
             raise
+    else:
+        raise
 
     # 如果设置了 num_sample 则随机采样以便快速计算 u 的 relative l2 error
     if num_sample is not None:
@@ -82,7 +130,6 @@ def relative_error_of_parameter(pinn, ref_data, num_sample=None, column_index=No
     # 下面希望拆分成逐列的形式，用于计算 relative l2 error
     # 如果 param 只有一列，则直接 flatten
     # 如果 param 是多列的，则分开 param 为逐列的形式，再 flatten
-    # 顺便还要将列 flatten
     if param.shape[1] == 1:
         param = param.flatten()
     elif param.shape[1] > 1:
@@ -91,7 +138,7 @@ def relative_error_of_parameter(pinn, ref_data, num_sample=None, column_index=No
         raise
 
     # 因为 net_param 一定是以单列的形式返回的
-    # 所以，想要知道有几个反演参数要计算，则判断它是否为 list/tuple，
+    # 所以，想要知道有几个反演参数要计算，则判断它是否为 list/tuple
     # 如果是，则逐列计算 error，如果不是，则直接计算 error
     if isinstance(param_pred, (list, tuple)):
         param_pred = [p_pred.detach().cpu().numpy().flatten() for p_pred in param_pred]
