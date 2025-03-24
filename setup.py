7c3a12e7a74cc8a038c180de3a1e9356e7032ec2
Guanyu191 <guanyu191@163.com>
2025-03-24 11:46:21 +0800

    0.1.3

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'torch',
-    ],
+    # install_requires=[
+    #     'numpy',
+    #     'scipy',
+    #     'matplotlib',
+    #     'torch',
+    # ],
     entry_points={
         'console_scripts': [
             'pdeasy-cli = pdeasy.cli:main',
