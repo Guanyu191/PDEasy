92ba08273dd97e4d782789da54d9b9fdfe1e819b
Guanyu191 <guanyu191@163.com>
2025-03-24 11:07:53 +0800

    0.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Guanyu Pan',
     author_email='guanyu191@163.com',
     description='Lightweight PINN & Operator PDE Solver for Research, Balancing Abstraction and Flexibility for Algorithm Innovation.',
-    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description=open('README.md', encoding='utf-8').read() if os.path.exists('README.md') else '',
     long_description_content_type='text/markdown',
     url='https://github.com/Guanyu191/PDEasy',
     classifiers=[
@@ -16,12 +16,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    # install_requires=[
-    #     'numpy',
-    #     'scipy',
-    #     'matplotlib',
-    #     'torch',
-    # ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'torch',
+    ],
     entry_points={
         'console_scripts': [
             'pdeasy-cli = pdeasy.cli:main',
